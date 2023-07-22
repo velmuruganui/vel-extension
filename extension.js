c1eb2ff17467e93db80822b2fa5c51dfc29bf48e
Velmurugan <velmuruganfrontenddeveloper@gmail.com>
2023-07-22 06:25:40 +0530

    Added Activity bar and Primary sidebar panel

--- extension.js
+++ extension.js
@@ -1,30 +1,39 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vel-extension" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
+	
 	let disposable = vscode.commands.registerCommand('vel-extension.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vel-extension!');
+		const treeDataProvider = new MyTreeDataProvider();
+	    vscode.window.createTreeView('myExtension', { treeDataProvider });
 	});
-
 	context.subscriptions.push(disposable);
+
+}
+
+
+class MyTreeDataProvider {
+	getTreeItem(element) { 
+	  return element;
+	}
+  
+	getChildren(element) {
+	  const items = [
+		new MyTreeItem('Item 1'),
+		new MyTreeItem('Item 2'),
+		new MyTreeItem('Item 3'),
+	  ];
+  
+	  return Promise.resolve(items);
+	}
+}
+
+class MyTreeItem extends vscode.TreeItem {
+	constructor(label) {
+	  super(label, vscode.TreeItemCollapsibleState.None);
+	}
 }
 
 
